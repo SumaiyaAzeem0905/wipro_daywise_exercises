98e0686b4a9bbd6f65b5e326717adb95396cfb63
Sumaiya Azeem <sumaiyaazeem01@gmail.com>
2025-08-18 11:00:38 +0530

    Angular Practice

---
+++ day24__18_08_2025/firstapp/src/main.ts
@@ -0,0 +1,8 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+
+import { SecondcompComponent } from './app/secondcomp/secondcomp.component';
+import { LoginComponent } from './app/login/login.component';
+
+bootstrapApplication(LoginComponent, appConfig)
+  .catch((err) => console.error(err));
